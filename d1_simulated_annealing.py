9c583c09895070c74786452dc0743bae1af5c7a1
HelinXu <xhl19@mails.tsinghua.edu.cn>
2021-12-04 12:13:23 +0800

    formatting

--- d1_simulated_annealing.py
+++ d1_simulated_annealing.py
@@ -11,42 +11,42 @@ from time import time
 
 # objective function
 def objective(x):
-	return -np.exp(-(x/30.)**2.0) * np.cos(x)
+    return -np.exp(-(x/30.)**2.0) * np.cos(x)
 
 # simulated annealing algorithm
 def simulated_annealing(objective, bounds, n_iterations, step_size, temp):
-	# generate an initial point
-	best = bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
-	# evaluate the initial point
-	best_eval = objective(best)
-	# current working solution
-	curr, curr_eval = best, best_eval
-	scores = list()
-	# run the algorithm
-	for i in range(n_iterations):
-		# take a step
-		candidate = curr + randn(len(bounds)) * step_size
-		# evaluate candidate point
-		candidate_eval = objective(candidate)
-		# check for new best solution
-		if candidate_eval < best_eval:
-			# store new best point
-			best, best_eval = candidate, candidate_eval
-			# keep track of scores
-			scores.append(best_eval)
-			# report progress
-			print('>%d f(%s) = %.5f' % (i, best, best_eval))
-		# difference between candidate and current point evaluation
-		diff = candidate_eval - curr_eval
-		# calculate temperature for current epoch
-		t = temp / float(i + 1)
-		# calculate metropolis acceptance criterion
-		metropolis = exp(-diff / t)
-		# check if we should keep the new point
-		if diff < 0 or rand() < metropolis:
-			# store the new current point
-			curr, curr_eval = candidate, candidate_eval
-	return [best, best_eval, scores]
+    # generate an initial point
+    best = bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
+    # evaluate the initial point
+    best_eval = objective(best)
+    # current working solution
+    curr, curr_eval = best, best_eval
+    scores = list()
+    # run the algorithm
+    for i in range(n_iterations):
+        # take a step
+        candidate = curr + randn(len(bounds)) * step_size
+        # evaluate candidate point
+        candidate_eval = objective(candidate)
+        # check for new best solution
+        if candidate_eval < best_eval:
+            # store new best point
+            best, best_eval = candidate, candidate_eval
+            # keep track of scores
+            scores.append(best_eval)
+            # report progress
+            print('>%d f(%s) = %.5f' % (i, best, best_eval))
+        # difference between candidate and current point evaluation
+        diff = candidate_eval - curr_eval
+        # calculate temperature for current epoch
+        t = temp / float(i + 1)
+        # calculate metropolis acceptance criterion
+        metropolis = exp(-diff / t)
+        # check if we should keep the new point
+        if diff < 0 or rand() < metropolis:
+            # store the new current point
+            curr, curr_eval = candidate, candidate_eval
+    return [best, best_eval, scores]
 
 
 if __name__ == '__main__':
