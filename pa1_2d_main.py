2f115704600579c8709f2886b52a5d3dd557ac82
HelinXu <xhl19@mails.tsinghua.edu.cn>
2021-12-05 19:47:44 +0800

    update

--- pa1_2d_main.py
+++ pa1_2d_main.py
@@ -1,4 +1,4 @@
-from function.d1_simulated_annealing import simulated_annealing
+from function.d2_simulated_annealing import simulated_annealing
 import argparse
 import os
 import numpy as np
@@ -11,8 +11,9 @@ import numpy as np
 from time import time
 
 # objective function
-def objective(x):
-    return -np.exp(-(x/30.)**2.0) * np.cos(x)
+def objective(x, y):
+    return -20.0 * exp(-0.2 * sqrt(0.5 * (x**2 + y**2)))-exp(0.5 * (cos(2 * 
+            pi * x)+cos(2 * pi * y))) + e + 20
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -20,21 +21,23 @@ if __name__ == '__main__':
     opt = parser.parse_args()
 
     os.system(f'mkdir {opt.o}')
+    r_min, r_max = -10, 10
     # seed the pseudorandom number generator
     seed(int(time()))
     # define range for input
-    bounds = asarray([[-30.0, 30.0]])
+    bounds = asarray([[r_min, r_max], [r_min, r_max]])
     # define the total iterations
-    n_iterations = 2000
+    n_iterations = 10000
     # define the maximum step size
-    step_size = 5.
+    step_size = 2.
     # initial temperature
-    temp = 10
+    temp = 100
+    print('Done!')
 
     result_dist_list = []
     for i in range(20):
         # perform the simulated annealing search
-        best, score, scores = simulated_annealing(objective, bounds, n_iterations, step_size, temp)
+        best, score, scores, points = simulated_annealing(objective, bounds, n_iterations, step_size, temp)
         result_dist_list.append(float(score))
 
     # get results and save to file
@@ -43,7 +46,7 @@ if __name__ == '__main__':
     mean_results = mean(result_dist_list)
     var_results = variance(result_dist_list)
     print(f'mean: {mean_results}, var: {var_results}')
-    f = open(os.path.join(opt.o, 'result_1d.txt'), 'a')
+    f = open(os.path.join(opt.o, 'result_2d.txt'), 'a')
     f.write(f'20 time resluts: {sorted(result_dist_list, key = lambda x:float(x))}')
     f.write(f'\nmean: {mean_results}, var: {var_results}')
     f.write('\n\n')
